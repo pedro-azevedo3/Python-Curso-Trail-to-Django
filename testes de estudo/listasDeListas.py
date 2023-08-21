ce2528c4dd7459912a4ae9d59b86fb5f0bbcab3b
Pedro <pedroazevedoteixeira7@gmail.com>
2023-08-21 10:20:41 -0300

    Segundo Commit

---
+++ testes de estudo/listasDeListas.py
@@ -0,0 +1,13 @@
+"""
+Lista de listas e seus índices
+"""
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
+
+print(salas[0][1])
